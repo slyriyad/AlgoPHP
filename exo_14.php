35cba0d065057cb899d868d3c291964be6588123
slyriyad <riyad25400@gmail.com>
2024-01-22 09:31:25 +0100

    rename files

---
+++ exo_14.php
@@ -0,0 +1,13 @@
+<h1>Exerice 14</h1>
+
+<p>Calculer l'âge exact d'une personne à partir de sa date de naissance (en années, mois, jours).</p>
+
+<h2>Resultat</h2>
+
+<?php
+
+$datebirth = new DateTimeImmutable('2022-01-22');
+$now = new DateTime("now");
+$interval = date_diff($datebirth, $now);
+
+echo "Age de la personne : ".$interval->format('%y ans %m mois %d jours ');
