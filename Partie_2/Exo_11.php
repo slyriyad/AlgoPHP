c32b2acce63495898bf41068bd6406cfb6341fac
slyriyad <riyad25400@gmail.com>
2024-01-25 11:27:02 +0100

    exo 11

--- Partie_2/Exo_11.php
+++ Partie_2/Exo_11.php
@@ -8,4 +8,19 @@ formaterDateFr("2018-02-23");</p>
 <h2>Resultat</h2>
 
 <?php
+setlocale(LC_TIME, 'fr_FR');
 
+echo formaterDateFr("2018-02-23");
+
+function formaterDateFr($dateString) {
+    $date = new DateTime($dateString); /* Conversion de la chaîne en objet DateTime */ 
+
+    // Créer un formateur de date avec la locale française
+    $formateur = new IntlDateFormatter('fr_FR', IntlDateFormatter::FULL, IntlDateFormatter::NONE);
+
+    // Formater la date en utilisant le formateur
+    $result = $formateur->format($date);
+
+    return $result;
+}
+?>
